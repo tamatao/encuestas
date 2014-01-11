7baa45742a0d2b102180f475e74003b02bcd3fb1
Armando <tamatao@gmail.com>
2014-01-11 01:42:50 -0600

    Muestra la lista de encuestas

--- surveys/public/js/views/dashboard/dashboard.js
+++ surveys/public/js/views/dashboard/dashboard.js
@@ -10,23 +10,27 @@ define([
       var self = this;
       $('body .container header h1').html('Dashboard');
       $(this.el).html('dashboard');
-      require(["libs/widgets/widget.table"], function(){
-        console.log('surveys table');
-        var tSurveys = $('<div/>').table({
-          btnNew: 'survey/add',
-          columns:[
-            {name:'name', label:'Nombre'},
-            {name:'description', label:'Descripcion'},
-            {name:'url', label:'ver encuesta', type:'link'}
-          ],
-          url:null,
-          data:[
-            {name:'Primera encuesta', 'description':'Esta es una encuesta sobre videojuegos', 'url':'surveys/test/0'},
-            {name:'Segunda encuesta', 'description':'Esta es una encuesta sobre animales favoritos', 'url':'surveys/test/0'},
-            {name:'Tercera encuesta', 'description':'Esta es una encuesta sobre peliculas favoritos', 'url':'surveys/test/0'}
-          ]
-        });
-        self.$el.empty().append(tSurveys);
+      self.collection.url = 'survey'
+      self.collection.fetch({
+        success: function() {
+          require(["libs/widgets/widget.table"], function(){
+            console.log('surveys table');
+            var tSurveys = $('<div/>').table({
+              btnNew: 'survey/add',
+              columns:[
+                {name:'name', label:'Nombre'},
+                {name:'description', label:'Descripcion'},
+                {name:'url', label:'ver encuesta', type:'link'}
+              ],
+              url:null,
+              collection:self.collection
+            });
+            self.$el.empty().append(tSurveys);
+          });
+        },
+        error: function() {
+          console.log('No se pudo cargar la collecion Survey');
+        }
       });
     }
   });
